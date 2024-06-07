6569a70010f6feb69ad04e646cde48ea729ad9c2
AnneEberhard <anne.eberhard@gmx.net>
2024-06-07 11:15:42 +0200

    sql added

--- todo/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Todo
-
-class TodoAdmin(admin.ModelAdmin):
-    list_display = ('created_at', 'title', 'user')
-
-
-admin.site.register(Todo,TodoAdmin)
--- up.bat
+++
@@ -1,4 +0,0 @@
-git add .
-git commit -m "%*"
-git push
-chrome "https://www.pythonanywhere.com/user/AnneEberhard/consoles/32230456/"
