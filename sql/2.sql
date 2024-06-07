11c8dc03cac863a3d5e7784620e6a8d1da3c0505
AnneEberhard <anne.eberhard@gmx.net>
2024-06-07 11:15:56 +0200

    sql added

---
+++ sql/2.sql
@@ -0,0 +1,2 @@
+INSERT INTO todo_todo (title, description, created_at, user_id)
+VALUES('test', 'über sql', '2024-06-07', 2);
---
+++ up.bat
@@ -0,0 +1,4 @@
+git add .
+git commit -m "%*"
+git push
+chrome "https://www.pythonanywhere.com/user/AnneEberhard/consoles/32230456/"
